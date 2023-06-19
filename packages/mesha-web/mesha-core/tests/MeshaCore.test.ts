0fa644f82f5dff7c4bd69cf9f819bb22649a7266
Alex Goodman <alexthegoodman@gmail.com>
2023-06-19 10:49:31 -0400

    init tdd

---
+++ packages/mesha-web/mesha-core/tests/MeshaCore.test.ts
@@ -0,0 +1,28 @@
+import { describe, expect, test } from "vitest";
+import MeshaCore from "../MeshaCore";
+
+describe("MeshaCore", () => {
+  test("should create MeshaCore class with appropriate properties", () => {
+    //instantiate class and check properties
+    const meshaCore = new MeshaCore();
+
+    expect(meshaCore).toBeDefined();
+    expect(meshaCore).toBeInstanceOf(MeshaCore);
+    expect(meshaCore).toHaveProperty("Helpers");
+    expect(meshaCore).toHaveProperty("Canvas");
+    expect(meshaCore).toHaveProperty("Scene");
+  });
+
+  test("initialize should attach to canvas element and create canvas and scene instances", () => {
+    //instantiate class and check properties
+    const meshaCore = new MeshaCore();
+    const canvasElement = document.createElement("canvas");
+
+    meshaCore.initialize(canvasElement);
+
+    expect(meshaCore.canvas).toBeDefined();
+    expect(meshaCore.canvas).toBeInstanceOf(MeshaCore.Canvas);
+    expect(meshaCore.scene).toBeDefined();
+    expect(meshaCore.scene).toBeInstanceOf(MeshaCore.Scene);
+  });
+});
---
+++ packages/mesha-web/mesha-core/tests/renderer/BasicPipeline.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, test } from "vitest";
+import BasicPipeline from "../../renderer/pipelines/BasicPipeline";
+
+describe("BasicPipeline", () => {
+  test("should create BasicPipeline class with appropriate properties", () => {
+    //instantiate class and check properties
+    const basicPipeline = new BasicPipeline();
+
+    expect(basicPipeline).toBeDefined();
+    expect(basicPipeline).toBeInstanceOf(BasicPipeline);
+    // expect(basicPipeline).toHaveProperty("name");
+  });
+});
